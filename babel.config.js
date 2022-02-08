a878723b12e5db98ea3a05527b726d7b74d4a71b
christiannicoletti <cwnicoletti@gmail.com>
2022-02-08 09:44:35 -0800

    add: initial commit, inspired by: @react-native-community/slider

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      "@babel/preset-flow",
+      'module:metro-react-native-babel-preset'
+      ],
+  };
+  
---
+++ src/js/Speedometer.tsx
@@ -0,0 +1,5 @@
+'use strict';
+
+import React from 'react';
+import {Image, Platform, StyleSheet} from 'react-native';
+
