06b55fe651c8f2f661f43c93e33ddc058d6ab8e2
christiannicoletti <cwnicoletti@gmail.com>
2022-02-18 10:55:57 -0800

    fix: useDidMountEffect code style

--- src/helper/useDidMountEffect.js
+++ src/helper/useDidMountEffect.js
@@ -1,11 +1,14 @@
-import { useEffect, useRef } from "react";
+import {useEffect, useRef} from 'react';
 
 const useDidMountEffect = (func, deps) => {
   const didMount = useRef(false);
 
   useEffect(() => {
-    if (didMount.current) func();
-    else didMount.current = true;
+    if (didMount.current) {
+      func();
+    } else {
+      didMount.current = true;
+    }
   }, deps);
 };
 
